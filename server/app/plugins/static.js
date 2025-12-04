3d6f3807aa9311b1f0a76a7135e8e97e102e7b70
Vishal Gautam <ivishalgautamm@gmail.com>
2025-12-04 17:15:12 +0530

    fixed static file serve

--- server/app/plugins/static.js
+++ server/app/plugins/static.js
@@ -1,12 +1,10 @@
 import fp from "fastify-plugin";
 import fastifyStatic from "@fastify/static";
-import { dirname, join } from "path";
-import { fileURLToPath } from "url";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
+import { join } from "path";
 
 export default fp(async (fastify) => {
   await fastify.register(fastifyStatic, {
     root: join(process.cwd(), "public"),
+    prefix: "/public/",
   });
 });
